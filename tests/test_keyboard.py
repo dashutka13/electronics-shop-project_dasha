c39158104eb8841eecc4dd46adceaf3444bc34f6
dashuta <it.dashutka@gmail.com>
2023-08-08 07:04:57 +0300

    add: class keyboard, class Mixinlang, test_keyboard

---
+++ tests/test_keyboard.py
@@ -0,0 +1,16 @@
+from src.keyboard import Keyboard
+
+
+def test_keyboard():
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+
+    assert str(kb) == "Dark Project KD87A"
+
+    assert str(kb.language) == "EN"
+
+    kb.change_lang()
+    assert str(kb.language) == "RU"
+
+    # Сделали RU -> EN -> RU
+    kb.change_lang().change_lang()
+    assert str(kb.language) == "RU"
